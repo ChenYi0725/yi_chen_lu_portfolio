49f887862719099c3b339d4ec9e3f912f8da8ee0
eason <eason.jshouse0725@gmail.com>
2025-08-25 10:00:47 +0800

    adjust header bar layout

--- lib/component/header_bar.dart
+++ lib/component/header_bar.dart
@@ -23,6 +23,10 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
     {'label': 'Resume', 'route': '/resume'},
   ];
 
+  Widget mobileLayout({required Widget child, required bool isMobile}) {
+    return !isMobile ? Center(child: child) : child;
+  }
+
   @override
   Widget build(BuildContext context) {
     final isMobile = Provider.of<ResponsiveProvider>(
@@ -30,82 +34,85 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
       listen: true,
     ).isMobile;
 
-    return Material(
-      color: Colors.transparent,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // 左側名稱
-                !isMobile
-                    ? Flexible(
-                        fit: FlexFit.loose,
-                        child: InkWell(
-                          onTap: () => context.go('/'),
-                          child: RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: 'YICHEN   LU',
-                                  style: headerBarNameStyle,
-                                ),
-                                TextSpan(text: "  "),
-                                TextSpan(
-                                  text: 'LIGHTING DESIGNER',
-                                  style: headerBarTitleStyle,
-                                ),
-                              ],
+    return Flexible(
+      child: Material(
+        color: Colors.transparent,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+              child: Row(
+                mainAxisAlignment: !isMobile
+                    ? MainAxisAlignment.spaceBetween
+                    : MainAxisAlignment.center,
+                children: [
+                  // 左側名稱
+                  !isMobile
+                      ? Flexible(
+                          fit: FlexFit.loose,
+                          child: InkWell(
+                            onTap: () => context.go('/'),
+                            child: RichText(
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: 'YICHEN   LU',
+                                    style: headerBarNameStyle,
+                                  ),
+                                  TextSpan(text: "  "),
+                                  TextSpan(
+                                    text: 'LIGHTING DESIGNER',
+                                    style: headerBarTitleStyle,
+                                  ),
+                                ],
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                              softWrap: false,
                             ),
+                          ),
+                        )
+                      : SizedBox.shrink(),
+
+                  Wrap(
+                    spacing: 24,
+                    children: navItems.map((item) {
+                      final isSelected = currentRoute == item['route'];
+                      return InkWell(
+                        onTap: () => onNavItemSelected(item['route']!),
+                        child: Container(
+                          constraints: const BoxConstraints(maxWidth: 100),
+                          padding: const EdgeInsets.symmetric(vertical: 4),
+                          decoration: isSelected
+                              ? const BoxDecoration(
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: Colors.white,
+                                      width: 2,
+                                    ),
+                                  ),
+                                )
+                              : null,
+                          child: Text(
+                            item['label']!,
                             overflow: TextOverflow.ellipsis,
                             softWrap: false,
+                            style: headerBarTextStyle.copyWith(
+                              fontWeight: isSelected
+                                  ? FontWeight.bold
+                                  : FontWeight.normal,
+                            ),
                           ),
                         ),
-                      )
-                    : SizedBox.shrink(),
-
-                // 導覽列
-                Wrap(
-                  spacing: 24,
-                  children: navItems.map((item) {
-                    final isSelected = currentRoute == item['route'];
-                    return InkWell(
-                      onTap: () => onNavItemSelected(item['route']!),
-                      child: Container(
-                        constraints: const BoxConstraints(maxWidth: 100),
-                        padding: const EdgeInsets.symmetric(vertical: 4),
-                        decoration: isSelected
-                            ? const BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(
-                                    color: Colors.white,
-                                    width: 2,
-                                  ),
-                                ),
-                              )
-                            : null,
-                        child: Text(
-                          item['label']!,
-                          overflow: TextOverflow.ellipsis,
-                          softWrap: false,
-                          style: headerBarTextStyle.copyWith(
-                            fontWeight: isSelected
-                                ? FontWeight.bold
-                                : FontWeight.normal,
-                          ),
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-              ],
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const Divider(height: 3, color: Colors.white54, thickness: 3),
-        ],
+            const Divider(height: 3, color: Colors.white54, thickness: 3),
+          ],
+        ),
       ),
     );
   }
