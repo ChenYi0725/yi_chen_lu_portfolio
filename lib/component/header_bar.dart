ae30c3ed8b05a3af4bc5e6e17d57463f6017d8d9
eason <eason.jshouse0725@gmail.com>
2026-08-31 16:21:14 +0800

    drawer

--- lib/component/header_bar.dart
+++ lib/component/header_bar.dart
@@ -1,5 +1,3 @@
-import 'dart:ui' show PointerDeviceKind;
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -8,7 +6,7 @@ import 'package:yi_chen_lu_protfolio/constant.dart';
 import '../provider/responsive_provider.dart';
 
 class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
-  static const double _scrollableHeaderBreakpoint = 1100;
+  static const double _compactHeaderBreakpoint = 1100;
 
   final String currentRoute;
   final Function(String) onNavItemSelected;
@@ -40,11 +38,10 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
       context,
       listen: true,
     ).isMobile;
-    final useScrollableHeader =
-        isMobile ||
-        MediaQuery.sizeOf(context).width < _scrollableHeaderBreakpoint;
+    final useCompactHeader =
+        isMobile || MediaQuery.sizeOf(context).width < _compactHeaderBreakpoint;
 
-    if (useScrollableHeader) {
+    if (useCompactHeader) {
       return Material(
         color: Colors.transparent,
         child: Column(
@@ -52,16 +49,17 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
           children: [
             SizedBox(
               height: 69,
-              child: ScrollConfiguration(
-                behavior: const _HeaderScrollBehavior(),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Row(
-                    children: [
-                      InkWell(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: InkWell(
                         onTap: () => context.go('/'),
                         child: RichText(
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
                           text: TextSpan(
                             children: [
                               TextSpan(
@@ -75,43 +73,19 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
                               ),
                             ],
                           ),
-                          softWrap: false,
                         ),
                       ),
-                      const SizedBox(width: 32),
-                      ...navItems.map((item) {
-                        final isSelected = currentRoute == item['route'];
-                        return Padding(
-                          padding: const EdgeInsets.only(right: 24),
-                          child: InkWell(
-                            onTap: () => onNavItemSelected(item['route']!),
-                            child: Container(
-                              padding: const EdgeInsets.symmetric(vertical: 4),
-                              decoration: isSelected
-                                  ? const BoxDecoration(
-                                      border: Border(
-                                        bottom: BorderSide(
-                                          color: Colors.white,
-                                          width: 2,
-                                        ),
-                                      ),
-                                    )
-                                  : null,
-                              child: Text(
-                                item['label']!,
-                                style: headerBarTextStyle.copyWith(
-                                  fontWeight: isSelected
-                                      ? FontWeight.bold
-                                      : FontWeight.normal,
-                                  fontSize: 14,
-                                ),
-                              ),
-                            ),
-                          ),
-                        );
-                      }),
-                    ],
-                  ),
+                    ),
+                    IconButton(
+                      tooltip: 'Open navigation',
+                      icon: const Icon(
+                        Icons.menu,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      onPressed: () => _showNavigationDrawer(context),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -203,18 +177,86 @@ class HeaderBar extends StatelessWidget implements PreferredSizeWidget {
     );
   }
 
+  Future<void> _showNavigationDrawer(BuildContext context) {
+    return showGeneralDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      barrierLabel: 'Close navigation',
+      barrierColor: Colors.black54,
+      transitionDuration: const Duration(milliseconds: 250),
+      pageBuilder: (dialogContext, animation, secondaryAnimation) {
+        return Align(
+          alignment: Alignment.centerRight,
+          child: Drawer(
+            backgroundColor: themeColor,
+            child: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 24),
+                    title: Text('YI-CHEN LU', style: headerBarNameStyle),
+                    subtitle: Text(
+                      'LIGHTING DESIGNER',
+                      style: headerBarTitleStyle,
+                    ),
+                    onTap: () {
+                      Navigator.of(dialogContext).pop();
+                      context.go('/');
+                    },
+                  ),
+                  const Divider(color: Colors.white38),
+                  Expanded(
+                    child: ListView(
+                      children: navItems.map((item) {
+                        final route = item['route']!;
+                        final isSelected = currentRoute == route;
+                        return ListTile(
+                          contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 24,
+                          ),
+                          selected: isSelected,
+                          selectedTileColor: Colors.white12,
+                          title: Text(
+                            item['label']!,
+                            style: headerBarTextStyle.copyWith(
+                              fontWeight: isSelected
+                                  ? FontWeight.bold
+                                  : FontWeight.normal,
+                            ),
+                          ),
+                          trailing: isSelected
+                              ? const Icon(
+                                  Icons.chevron_right,
+                                  color: Colors.white70,
+                                )
+                              : null,
+                          onTap: () {
+                            Navigator.of(dialogContext).pop();
+                            onNavItemSelected(route);
+                          },
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(1, 0),
+            end: Offset.zero,
+          ).animate(CurvedAnimation(parent: animation, curve: Curves.easeOut)),
+          child: child,
+        );
+      },
+    );
+  }
+
   @override
   Size get preferredSize => const Size.fromHeight(72);
-}
-
-class _HeaderScrollBehavior extends MaterialScrollBehavior {
-  const _HeaderScrollBehavior();
-
-  @override
-  Set<PointerDeviceKind> get dragDevices => const {
-    PointerDeviceKind.touch,
-    PointerDeviceKind.mouse,
-    PointerDeviceKind.stylus,
-    PointerDeviceKind.trackpad,
-  };
 }
